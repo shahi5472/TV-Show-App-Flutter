ae5c3494285830b7136ac5b0635df4ff3fc532a2
SHAHi <shahi@SHAHis-MacBook-Pro.local>
2021-01-29 21:33:58 +0600

    string add in strings class

---
+++ lib/src/strings.dart
@@ -0,0 +1,13 @@
+class Strings {
+  Strings._();
+
+  static final String app_name = 'TV Show App';
+  static final String tv_shows_most_popular = 'TV Shows | Most Popular';
+  static final String read_more = 'Read More';
+  static final String read_less = 'Read Less';
+  static final String u2022 = '.';
+  static final String website = 'Website';
+  static final String episodes = 'Episodes';
+  static final String watchlist = 'Watchlist';
+  static final String search_tv_show = 'Search TV Show';
+}
