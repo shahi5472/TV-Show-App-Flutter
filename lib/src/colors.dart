1da830a9ae1e439981bbc21e0b512f99db99fc7d
SHAHi <shahi@SHAHis-MacBook-Pro.local>
2021-01-29 21:30:38 +0600

    color add in color class

---
+++ lib/src/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+  Colors._();
+
+  static final Color purple_200 = Color(0xFF141A31);
+  static final Color purple_500 = Color(0xFF081029);
+  static final Color purple_700 = Color(0xFF0097FB);
+  static final Color teal_200 = Color(0xFF03DAC5);
+  static final Color teal_700 = Color(0xFF018786);
+  static final Color black = Color(0xFF000000);
+  static final Color white = Color(0xFFFFFFFF);
+
+  static final Color colorTextIcons = Color(0xFFFFFFFF);
+  static final Color colorPrimaryLight = Color(0xFF1E2746);
+  static final Color colorTextPrimary = Color(0xFFCBD5ED);
+  static final Color colorTextSecondary = Color(0xFF141A31);
+  static final Color colorTextOther = Color(0xFFFAD303);
+  static final Color colorThemeExtra = Color(0xFFFF5252);
+}
