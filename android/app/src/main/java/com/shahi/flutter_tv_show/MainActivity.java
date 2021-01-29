a202623cc9f2704c168a2946dea9a218a366dadb
SHAHi <shahi@SHAHis-MacBook-Pro.local>
2021-01-29 21:24:58 +0600

    file create

---
+++ android/app/src/main/java/com/shahi/flutter_tv_show/MainActivity.java
@@ -0,0 +1,6 @@
+package com.shahi.flutter_tv_show;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tv_show/screens/main_screen.dart';
+import 'package:flutter_tv_show/src/size_config.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return OrientationBuilder(
+          builder: (context, orientation) {
+            SizeConfig().init(constraints, orientation);
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'TV Shows',
+              home: MainScreen(),
+            );
+          },
+        );
+      },
+    );
+  }
+}
